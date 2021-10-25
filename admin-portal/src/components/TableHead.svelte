c290f6111c9274b8af84faaa9814d4ccc5ee1c6e
Farid Salau <max.zip848@gmail.com>
2021-10-25 09:43:34 -0500

    Create 'Table' component

---
+++ admin-portal/src/components/TableHead.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let titles: string[];
+</script>
+
+<thead class="bg-gray-50">
+	<tr>
+		{#each titles as title}
+			<th
+				scope="col"
+				class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+			>
+				{title}
+			</th>
+		{/each}
+	</tr>
+</thead>
--- admin-portal/src/routes/rewards/index.svelte
+++ admin-portal/src/routes/rewards/index.svelte
@@ -1,1 +1,5 @@
 <!-- allow user to update reward, delete reward, pause reward?, see most redeemed rewards, view previous rewards -->
+<script lang="ts">
+	import Table from '../../components/Table.svelte';
+	const titles: string[] = [];
+</script>
