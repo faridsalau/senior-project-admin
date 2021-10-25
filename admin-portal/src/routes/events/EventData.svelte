0404e179f0afeff941eed8e1524ce77a9d1b31d7
Farid Salau <max.zip848@gmail.com>
2021-10-25 09:26:28 -0500

    Create 'EventData' component for 'events' page

---
+++ admin-portal/src/routes/events/EventData.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let name: string;
+	export let date: string;
+	export let status: string;
+	export let attendance: number;
+	export let points: number;
+</script>
+
+<tr>
+	<td class="px-6 py-4 whitespace-nowrap">
+		<div class="flex items-center">
+			<div class="ml-4">
+				<div class="text-sm font-medium text-gray-900">{name}</div>
+				<div class="text-sm text-gray-500">{date}</div>
+			</div>
+		</div>
+	</td>
+	<td class="px-6 py-4 whitespace-nowrap">
+		<div class="text-sm text-gray-900">{attendance}</div>
+	</td>
+	<td class="px-6 py-4 whitespace-nowrap">
+		<span
+			class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+		>
+			{status}
+		</span>
+	</td>
+	<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"> {points} </td>
+	<td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+		<button type="text" class="text-red-600 hover:text-red-900">Delete</button>
+	</td>
+</tr>
--- admin-portal/src/routes/events/index.svelte
+++ admin-portal/src/routes/events/index.svelte
@@ -1,1 +1,71 @@
 <!-- show previous events, how many people attended, how many points were redeemed -->
+<script lang="ts">
+	import EventData from './EventData.svelte';
+</script>
+
+<div class="m-4 bg-gray-100">
+	<div class="flex flex-col">
+		<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+			<div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+				<div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+					<table class="min-w-full divide-y divide-gray-200">
+						<thead class="bg-gray-50">
+							<tr>
+								<th
+									scope="col"
+									class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+								>
+									Name and Date
+								</th>
+								<th
+									scope="col"
+									class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+								>
+									Attendance
+								</th>
+								<th
+									scope="col"
+									class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+								>
+									Status
+								</th>
+								<th
+									scope="col"
+									class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+								>
+									Points redeemed
+								</th>
+								<th scope="col" class="relative px-6 py-3">
+									<span class="sr-only">Delete</span>
+								</th>
+							</tr>
+						</thead>
+						<tbody class="bg-white divide-y divide-gray-200">
+							<EventData
+								name="Open Access + The Humanities"
+								date="10/19"
+								status="active"
+								attendance={34}
+								points={170000}
+							/>
+							<EventData
+								name="Open Access + The Humanities"
+								date="10/19"
+								status="active"
+								attendance={34}
+								points={170000}
+							/>
+							<EventData
+								name="Open Access + The Humanities"
+								date="10/19"
+								status="active"
+								attendance={34}
+								points={170000}
+							/>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
