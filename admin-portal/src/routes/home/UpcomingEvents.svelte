b21fa5236d29ebc97ab543ca005183c356fb7e8c
Farid Salau <max.zip848@gmail.com>
2021-11-20 13:09:47 -0600

    Add 'no upcoming events'

--- admin-portal/src/routes/home/UpcomingEvents.svelte
+++ admin-portal/src/routes/home/UpcomingEvents.svelte
@@ -7,7 +7,7 @@
 	export let loading: boolean;
 </script>
 
-{#if events.length > 0}
+{#if events.length > 0 && !loading}
 	<div in:fade class="m-12 p-4 bg-gray-200 rounded">
 		<h1 class="text-3xl text-center">Upcoming Events</h1>
 		{#each events as event}
@@ -17,5 +17,9 @@
 {:else if loading}
 	<div class="flex justify-center m-4">
 		<Circle size="60" color="#c9dcfd" unit="px" />
+	</div>
+{:else if !loading}
+	<div class="flex justify-center mt-4 text-gray-600 shadow p-4 bg-gray-100">
+		<p>No upcoming events :)</p>
 	</div>
 {/if}
