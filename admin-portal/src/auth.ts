db80cabee5dfacc7ca5d47652a522d61d44bcc7b
Farid Salau <max.zip848@gmail.com>
2021-11-21 00:55:30 -0600

    Add stub for events cronjob

--- admin-portal/src/auth.ts
+++ admin-portal/src/auth.ts
@@ -4,6 +4,7 @@ import { onAuthStateChanged, sendSignInLinkToEmail } from 'firebase/auth';
 import { auth, db } from './firebase';
 import type { User } from 'firebase/auth';
 import { collection, doc, getDoc, setDoc } from '@firebase/firestore';
+import hash from 'object-hash';
 import useAuthRedirect from './useAuthRedirect';
 
 type Auth = {
@@ -37,6 +38,36 @@ const authContext = () => {
 		const adminsRef = collection(db, 'admins');
 		await setDoc(doc(adminsRef, email), { email });
 	};
+
+	// const addEvents = async () => {
+	// 	const eventsRef = collection(db, 'events');
+	// 	const events = [
+	// 		{
+	// 			title: 'STOMP',
+	// 			url: 'https://events.olemiss.edu/event.php?value=000000033540',
+	// 			month: 'Nov',
+	// 			day: '29'
+	// 		},
+	// 		{
+	// 			title: 'Postmodern Jukebox',
+	// 			url: 'https://events.olemiss.edu/event.php?value=000000033548',
+	// 			month: 'Dec',
+	// 			day: '4'
+	// 		}
+	// 	];
+	// 	const ret = [];
+	// 	for (let i = 0; i < events.length; ++i) {
+	// 		const id = hash(events[i]);
+	// 		const obj = {
+	// 			...events[i],
+	// 			id,
+	// 			points: 500,
+	// 			active: true,
+	// 			attendance: 0
+	// 		};
+	// 		await setDoc(doc(eventsRef, id), obj);
+	// 	}
+	// };
 
 	const hasAdminEntry = async (email: string) => {
 		const adminsRef = doc(db, 'admins', email);
