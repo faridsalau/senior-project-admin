0ce366477cd0faccf398f790a6d8375bf9c7a223
Farid Salau <max.zip848@gmail.com>
2021-11-26 00:28:40 -0600

    Allow search by users email or name

--- admin-portal/src/routes/users/index.svelte
+++ admin-portal/src/routes/users/index.svelte
@@ -4,15 +4,17 @@
 	import User from './User.svelte';
 	const { fetchUsers } = authProvider;
 	let users = [];
+	let searchBy = 1;
 
 	onMount(async () => {
 		users = await fetchUsers();
 	});
 
 	let searchTerm = '';
-	$: results = users.filter(
-		(item) => item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
-	);
+	$: results =
+		searchBy === 1
+			? users.filter((item) => item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1)
+			: users.filter((item) => item.email.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1);
 
 	const src =
 		'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60';
@@ -23,8 +25,19 @@
 		bind:value={searchTerm}
 		class="focus:outline-none"
 		type="text"
-		placeholder="Search by users name"
+		placeholder={`Search by user's ${searchBy === 1 ? 'name' : 'email'}`}
 	/>
+</div>
+<div class="flex justify-end w-11/12 m-4">
+	<p class="mr-2">Search By:</p>
+	<label class="mr-2">
+		<input type="radio" bind:group={searchBy} name="searchBy" value={1} />
+		Name
+	</label>
+	<label>
+		<input type="radio" bind:group={searchBy} name="searchBy" value={2} />
+		Email
+	</label>
 </div>
 
 {#each results as user}
